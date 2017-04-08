dc6f8041532441203f83e4a9c3433a443bba4274
BharadwajMP <bharadwaj.mp.97@gmail.com>
2017-04-08 13:01:46 +0530

    Add files via upload

---
+++ mySox[1].py
@@ -0,0 +1,20 @@
+from subprocess import call
+import speech_recognition as sr
+
+r=sr.Recognizer()
+# sensitivity of silence recognition
+threshold = '30%'
+
+if  __name__ == "__main__":
+        output_filename = 'rec.wav'
+        cmd = ['rec', '-c', '1', output_filename, 'rate', '8k', 'silence', '1', '0.1', threshold, '1', '3.0', threshold]
+        call(cmd)
+	with sr.WavFile("rec.wav")as source:
+		audio=r.record(source)
+	try:
+		text=r.recognize_google(audio)
+	except sr.UnknownValueError:
+		print("Google Speech Recognition could not understand audio")
+	except sr.RequestError as e:
+	        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+	print "You said :"+text
